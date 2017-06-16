cf0dd1d7af19bebee85018950a845026cebb4b4c
ChunweiDu <duchunwei_it@163.com>
2017-06-16 11:58:32 +0800

    一个不错的WebView封装库的示例Demo

---
+++ app/src/main/java/com/dcw/agentwebsimple/MainActivity.java
@@ -0,0 +1,131 @@
+package com.dcw.agentwebsimple;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.just.library.AgentWeb;
+import com.just.library.ChromeClientCallbackManager;
+import com.just.library.LogUtils;
+
+public class MainActivity extends AppCompatActivity {
+
+    protected AgentWeb mAgentWeb;
+    private LinearLayout mLinearLayout;
+
+    private Toolbar mToolbar;
+    private TextView mTitleTextView;
+    private AlertDialog mAlertDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mLinearLayout = (LinearLayout) findViewById(R.id.container);
+        mToolbar = (Toolbar) this.findViewById(R.id.toolbar);
+        mToolbar.setTitleTextColor(Color.WHITE);
+        mToolbar.setTitle("");
+        mTitleTextView = (TextView) this.findViewById(R.id.toolbar_title);
+        this.setSupportActionBar(mToolbar);
+        if (getSupportActionBar() != null)
+            // Enable the Up button
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                showDialog();
+            }
+        });
+
+        mAgentWeb = AgentWeb.with(this)//
+                .setAgentWebParent(mLinearLayout, new LinearLayout.LayoutParams(-1, -1))//
+                .useDefaultIndicator()//
+                .defaultProgressBarColor()
+                .setReceivedTitleCallback(mCallback)
+                .setSecutityType(AgentWeb.SecurityType.strict)
+                .createAgentWeb()//
+                .ready()
+                .go("https://m.jd.com/");
+    }
+
+
+    private void showDialog() {
+
+        if (mAlertDialog == null)
+            mAlertDialog = new AlertDialog.Builder(this)
+                    .setMessage("您确定要关闭该页面吗?")
+                    .setNegativeButton("再逛逛", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            if (mAlertDialog != null)
+                                mAlertDialog.dismiss();
+                        }
+                    })//
+                    .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                            if (mAlertDialog != null)
+                                mAlertDialog.dismiss();
+                            finish();
+                        }
+                    }).create();
+        mAlertDialog.show();
+
+    }
+
+    private ChromeClientCallbackManager.ReceivedTitleCallback mCallback = new ChromeClientCallbackManager.ReceivedTitleCallback() {
+        @Override
+        public void onReceivedTitle(WebView view, String title) {
+            if (mTitleTextView != null)
+                mTitleTextView.setText(title);
+        }
+    };
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        if (mAgentWeb.handleKeyEvent(keyCode, event)) {
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        LogUtils.i("Info", "result:" + requestCode + " result:" + resultCode);
+        mAgentWeb.uploadFileResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mAgentWeb.destroy();
+    }
+
+}
